ab84e94cbb0b95b3130a3ace7517ca434efcb76d
Vladyslav Serdiuk <vlad6704@gmail.com>
2019-06-13 19:51:45 +0300

    all_controll

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
 window.onload = function () {
-    draw_grid();
-   
+    tetris.draw_grid();
+    tetris.start();
 }
 
 let tetris = {};
 tetris.occupiedCell = [];
 tetris.component = [];
+tetris.numbCurrentFigure = 0;
+tetris.size = {
+    rows: 19,
+    columns : 11
+}
 tetris.color = {
     diapazon: {
         r: {
@@ -35,13 +40,25 @@ tetris.getColor = function () {
 
 tetris.addComponent = function (nameComponent, posX, posY) {
     if (nameComponent == 'O') {
-        tetris.component.push(new Figure_box(posX, posY, this.getColor()));
+        return new_length = tetris.component.push(new Figure_box(posX, posY, this.getColor()));
     }
     if (nameComponent == 'I') {
-        tetris.component.push(new Figure_I(posX, posY, this.getColor()));
+        return new_length = tetris.component.push(new Figure_I(posX, posY, this.getColor()));
     }
     if (nameComponent == 'J') {
-        tetris.component.push(new Figure_J(posX, posY, this.getColor()));
+        return new_length = tetris.component.push(new Figure_J(posX, posY, this.getColor()));
+    }
+    if (nameComponent == 'L') {
+        return new_length = tetris.component.push(new Figure_L(posX, posY, this.getColor()));
+    }
+    if (nameComponent == 'T') {
+        return new_length = tetris.component.push(new Figure_T(posX, posY, this.getColor()));
+    }
+    if (nameComponent == 'S') {
+        return new_length = tetris.component.push(new Figure_S(posX, posY, this.getColor()));
+    }
+    if (nameComponent == 'Z') {
+        return new_length = tetris.component.push(new Figure_Z(posX, posY, this.getColor()));
     }
 }
 
@@ -60,12 +77,44 @@ tetris.isFreeSpace = function (ArrObjPosXPosY) {
     return return_bool;
 }
 
+tetris.start = function () {
+    this.addRandFigure(4, 0);
+    this.addEventListener();
+    var timerId = setInterval(function () {
+        tetris.component[tetris.numbCurrentFigure].muveDown();
+    }, 1000);
+    
+}
 
+tetris.setNumbCurrentFigure = function (numbCurrentFigure) {
+    this.numbCurrentFigure = numbCurrentFigure;
+}
 
+tetris.addRandFigure = function (posX,posY) {
+    let rand_numb = Math.round(Math.random() * (7 - 1) + 1);
+    let new_length = 0;
+    if (rand_numb == 1) new_length = tetris.addComponent('I', posX, posY);
+    if (rand_numb == 2) new_length = tetris.addComponent('J', posX, posY);
+    if (rand_numb == 3) new_length = tetris.addComponent('O', posX, posY);
+    if (rand_numb == 4) new_length = tetris.addComponent('L', posX, posY);
+    if (rand_numb == 5) new_length = tetris.addComponent('T', posX, posY);
+    if (rand_numb == 6) new_length = tetris.addComponent('S', posX, posY);
+    if (rand_numb == 7) new_length = tetris.addComponent('Z', posX, posY);
+    this.setNumbCurrentFigure(new_length - 1);
+}
 
-function draw_grid(rows, columns) {
-    if (rows === undefined) rows = 19;
-    if (columns === undefined) columns = 11;
+tetris.addEventListener = function () {
+    addEventListener("keydown", function (event) {
+        if (event.keyCode == 83) tetris.component[tetris.numbCurrentFigure].muveDown();
+        if (event.keyCode == 68) tetris.component[tetris.numbCurrentFigure].muveRight();
+        if (event.keyCode == 65) tetris.component[tetris.numbCurrentFigure].muveLeft();
+        if (event.keyCode == 87) tetris.component[tetris.numbCurrentFigure].rotate();
+    });
+}
+
+tetris.draw_grid = function (rows, columns) {
+    if (rows === undefined) rows = this.size.rows;
+    if (columns === undefined) columns = this.size.columns;
 
     for (let i = 1; i < columns; i++) {
         let div = document.createElement('div');
@@ -120,12 +169,12 @@ Box.prototype.setColor = function (color) {
 Box.prototype.setPosX = function (posX) {
     tetris.occupiedCell[this.id].posX = posX;
     this.posX = posX;
-    this.div.style.left = (100 / 11) * posX + '%';
+    this.div.style.left = (100 / tetris.size.columns) * posX + '%';
 }
 Box.prototype.setPosY = function (posY) {
     tetris.occupiedCell[this.id].posY = posY;
     this.posY = posY;
-    this.div.style.top = (100 / 19) * posY + '%';
+    this.div.style.top = (100 / tetris.size.rows) * posY + '%';
 }
 
 Box.prototype.setPosX_Y = function (posX, posY) {
@@ -136,6 +185,8 @@ Box.prototype.setPosX_Y = function (posX, posY) {
 
 
 function Figure(posX, posY, color) {
+    this.id = Figure.id;
+    Figure.id++;
     this.posX = posX;
     this.posY = posY;
     this.color = color;
@@ -146,13 +197,14 @@ function Figure(posX, posY, color) {
         new Box(color, posX, posY, '', '.container')
 
     ];
-
-
-
-
+    this.map = [];
+    this.number_current_map = 0;
+    this.frozen = false;
 }
 
-Figure.prototype.muve = function (arrObjNewPosXPoxY) {
+Figure.id = 0;
+
+Figure.prototype.setPosForSlug = function (arrObjNewPosXPoxY) {
     this.slug.forEach(function (slug_item, item) {
         slug_item.setPosX(arrObjNewPosXPoxY[item].posX);
         slug_item.setPosY(arrObjNewPosXPoxY[item].posY);
@@ -161,37 +213,121 @@ Figure.prototype.muve = function (arrObjNewPosXPoxY) {
 }
 
 Figure.prototype.muveDown = function () {
-    let ShiftPos = this.getShiftPos(0, 1);
+    let ShiftPos = this.getShiftPos(0, 1, this.slug);
     let ArrObjWithoutCurPos = this.retArrObjWithoutCurPos(ShiftPos);
     if (tetris.isFreeSpace(ArrObjWithoutCurPos)) {
- 
-        this.muve(ShiftPos);
-    } 
+        let cross = this.crossBorder(ShiftPos);
+        if (cross == false) {
+            this.setPosForSlug(ShiftPos);
+        } 
+         else if (cross == 'bottom') {
+            this.freeze();
+        }
+    }
+    else this.freeze();
 }
 
 Figure.prototype.muveRight = function () {
-    let ShiftPos = this.getShiftPos(1, 0);
+    let ShiftPos = this.getShiftPos(1, 0, this.slug);
     let ArrObjWithoutCurPos = this.retArrObjWithoutCurPos(ShiftPos);
     if (tetris.isFreeSpace(ArrObjWithoutCurPos)) {
-
-        this.muve(ShiftPos);
+        if (this.crossBorder(ShiftPos) == false) {
+            this.setPosForSlug(ShiftPos);
+        }
     }
 }
 
 Figure.prototype.muveLeft = function () {
-    let ShiftPos = this.getShiftPos(-1, 0);
+    let ShiftPos = this.getShiftPos(-1, 0, this.slug);
     let ArrObjWithoutCurPos = this.retArrObjWithoutCurPos(ShiftPos);
     if (tetris.isFreeSpace(ArrObjWithoutCurPos)) {
-
-        this.muve(ShiftPos);
+        if (this.crossBorder(ShiftPos) == false) {
+            this.setPosForSlug(ShiftPos);
+        }
     }
 }
 
-Figure.prototype.getShiftPos = function (shiftX, shiftY) {
+Figure.prototype.rotate = function () {
+    let number_map = this.getNumberNextMap();
+    let arrObjPosXPosY = this.getArrObjPosAcordingMap(number_map);
+    let ArrObjWithoutCurPos = this.retArrObjWithoutCurPos(arrObjPosXPosY);
+    if (tetris.isFreeSpace(ArrObjWithoutCurPos)) {
+        let cross = this.crossBorder(arrObjPosXPosY);
+        if (cross == false) {
+            this.setPosForSlug(arrObjPosXPosY);
+            this.number_current_map = number_map;
+
+        }
+        else if (cross == 'right') {
+            for (let i = 1; i <= 2; i++) {
+                let ShiftPos = this.getShiftPos(i * -1, 0, arrObjPosXPosY);
+                let ArrObjWithoutCurPos = this.retArrObjWithoutCurPos(ShiftPos);
+                if (tetris.isFreeSpace(ArrObjWithoutCurPos)) {
+                    if (this.crossBorder(ShiftPos) == false) {
+                        this.setPosForSlug(ShiftPos);
+                        this.number_current_map = number_map;
+                        break;
+                    }
+                }
+            }
+        }
+        else if (cross == 'left') {
+            for (let i = 1; i <= 2; i++) {
+                let ShiftPos = this.getShiftPos(i * 1, 0, arrObjPosXPosY);
+                let ArrObjWithoutCurPos = this.retArrObjWithoutCurPos(ShiftPos);
+                if (tetris.isFreeSpace(ArrObjWithoutCurPos)) {
+                    if (this.crossBorder(ShiftPos) == false) {
+                        this.setPosForSlug(ShiftPos);
+                        this.number_current_map = number_map;
+                        break;
+                    }
+                }
+            }
+        }
+        else if (cross == 'bottom') {
+            this.freeze();
+        }
+    }
+
+
+}
+
+Figure.prototype.freeze = function () {
+    this.frozen = true;
+    tetris.addRandFigure(4,0);
+}
+
+Figure.prototype.getArrObjPosAcordingMap = function (number_map) {
+    let arrObjPosXPoxY = [];
+    this.map[number_map].forEach(function (map_item, i) {
+        arrObjPosXPoxY[i] = {
+            posX: this.slug[i].posX + map_item.posX,
+            posY: this.slug[i].posY + map_item.posY,
+
+        }
+
+    }, this);
+    return arrObjPosXPoxY;
+}
+
+Figure.prototype.crossBorder = function (arrObjPosXPosY) {
+    if (arrObjPosXPosY.some(item => item.posX == -1)) return 'left';
+    else if (arrObjPosXPosY.some(item => item.posX == tetris.size.columns)) return 'right';
+    else if (arrObjPosXPosY.some(item => item.posY == tetris.size.rows)) return 'bottom';
+    else return false;
+}
+
+Figure.prototype.getNumberNextMap = function () {
+    let number_next_map = this.number_current_map + 1;
+    if (number_next_map >= this.map.length) number_next_map = 0;
+    return number_next_map;
+}
+
+Figure.prototype.getShiftPos = function (shiftX, shiftY, arrObjPosXPosY) {
     let retArrObjPosXPosY = [
 
     ];
-    this.slug.forEach(function (slug_item, i) {
+    arrObjPosXPosY.forEach(function (slug_item, i) {
         retArrObjPosXPosY[i] = {
             posX: slug_item.posX + shiftX,
             posY: slug_item.posY + shiftY,
@@ -235,11 +371,23 @@ function Figure_I(posX, posY, color) {
     this.slug[2].setPosX_Y(posX, posY + 2);
     this.slug[3].setPosX_Y(posX, posY + 3);
 
+    this.map[0] = [{ posX: -2, posY: -2 },
+                   { posX: -1, posY: -1 },
+                   { posX: 0, posY: 0 },
+                   { posX: +1, posY: +1 },
+    ];
+    this.map[1] = [{ posX: 2, posY: 2 },
+                   { posX: 1, posY: 1 },
+                   { posX: 0, posY: 0 },
+                   { posX: -1, posY: -1},
+    ];
+    
+    
 
 }
 
 Figure_I.prototype = Object.create(Figure.prototype);
-Figure_I.prototype.constructor = Figure_box;
+Figure_I.prototype.constructor = Figure_I;
 
 function Figure_J(posX, posY, color) {
     Figure.apply(this, arguments);
@@ -248,21 +396,167 @@ function Figure_J(posX, posY, color) {
     this.slug[2].setPosX_Y(posX, posY + 2);
     this.slug[3].setPosX_Y(posX - 1, posY + 2);
 
-
+    this.map[0] = [{
+        posX: 1, posY: -1
+    },
+        { posX: 0, posY: 0 },
+        { posX: -1, posY: 1 },
+        { posX: -2, posY: 0 },
+    ];
+    this.map[1] = [{ 
+      posX: 1, posY: 1 },
+    { posX: 0, posY: 0 },
+    { posX: -1, posY: -1 },
+    { posX: 0, posY: -2 },
+    ];
+    this.map[2] = [{
+      posX: -1, posY: 1    },
+    { posX: 0, posY: 0 },
+    { posX: 1, posY: -1 },
+    { posX: 2, posY: 0 },
+    ];
+    this.map[3] = [{
+        posX: -1, posY: -1    },
+    { posX: 0, posY: 0 },
+    { posX: 1, posY: 1 },
+    { posX: 0, posY: 2 },
+    ];
 }
 
 Figure_J.prototype = Object.create(Figure.prototype);
-Figure_J.prototype.constructor = Figure_box;
+Figure_J.prototype.constructor = Figure_J;
 
-console.time('df');
-tetris.addComponent('J', 1, 0);
-tetris.addComponent('O', 0, 6);
-tetris.addComponent('O', 4, 0);
-tetris.addComponent('O', 4, 0);
-console.timeEnd('df')
+function Figure_L(posX, posY, color) {
+    Figure.apply(this, arguments);
+    this.slug[0].setPosX_Y(posX, posY);
+    this.slug[1].setPosX_Y(posX, posY + 1);
+    this.slug[2].setPosX_Y(posX, posY + 2);
+    this.slug[3].setPosX_Y(posX + 1, posY + 2);
 
-addEventListener("keydown", function (event) {
-    if (event.keyCode == 83)    tetris.component[0].muveDown();
-    if (event.keyCode == 68)    tetris.component[0].muveRight();
-    if (event.keyCode == 65)    tetris.component[0].muveLeft();
-});
+    this.map[0] = [{
+        posX: 1, posY: -1
+    },
+    { posX: 0, posY: 0 },
+    { posX: -1, posY: 1 },
+    { posX: 0, posY: 2 },
+    ];
+    this.map[1] = [{
+        posX: 1, posY: 1
+    },
+    { posX: 0, posY: 0 },
+    { posX: -1, posY: -1 },
+    { posX: -2, posY: 0 },
+    ];
+    this.map[2] = [{
+        posX: -1, posY: 1
+    },
+    { posX: 0, posY: 0 },
+    { posX: 1, posY: -1 },
+    { posX: 0, posY: -2 },
+    ];
+    this.map[3] = [{
+        posX: -1, posY: -1
+    },
+    { posX: 0, posY: 0 },
+    { posX: 1, posY: 1 },
+    { posX: 2, posY: 0 },
+    ];
+}
+
+Figure_L.prototype = Object.create(Figure.prototype);
+Figure_L.prototype.constructor = Figure_L;
+
+function Figure_T(posX, posY, color) {
+    Figure.apply(this, arguments);
+    this.slug[0].setPosX_Y(posX, posY);
+    this.slug[1].setPosX_Y(posX, posY + 1);
+    this.slug[2].setPosX_Y(posX + 1, posY + 1);
+    this.slug[3].setPosX_Y(posX , posY + 2);
+
+    this.map[0] = [{
+        posX: 1, posY: -1
+    },
+    { posX: 0, posY: 0 },
+    { posX: 1, posY: 1 },
+    { posX: -1, posY: 1 },
+    ];
+    this.map[1] = [{
+        posX: 1, posY: 1
+    },
+    { posX: 0, posY: 0 },
+    { posX: -1, posY: 1 },
+    { posX: -1, posY: -1 },
+    ];
+    this.map[2] = [{
+        posX: -1, posY: 1
+    },
+    { posX: 0, posY: 0 },
+    { posX: -1, posY: -1 },
+    { posX: 1, posY: -1 },
+    ];
+    this.map[3] = [{
+        posX: -1, posY: -1
+    },
+    { posX: 0, posY: 0 },
+    { posX: 1, posY: -1 },
+    { posX: 1, posY: 1 },
+    ];
+}
+
+Figure_T.prototype = Object.create(Figure.prototype);
+Figure_T.prototype.constructor = Figure_T;
+
+function Figure_S(posX, posY, color) {
+    Figure.apply(this, arguments);
+    this.slug[0].setPosX_Y(posX + 1, posY);
+    this.slug[1].setPosX_Y(posX, posY );
+    this.slug[2].setPosX_Y(posX , posY + 1);
+    this.slug[3].setPosX_Y(posX - 1, posY + 1);
+
+    this.map[0] = [{
+        posX: 0, posY: -2
+    },
+    { posX: -1, posY: -1 },
+    { posX: 0, posY: 0 },
+    { posX: -1, posY: 1 },
+    ];
+    this.map[1] = [{
+        posX: 0, posY: 2
+    },
+    { posX: 1, posY: 1 },
+    { posX: 0, posY: 0 },
+    { posX: 1, posY: -1 },
+    ];
+
+}
+
+Figure_S.prototype = Object.create(Figure.prototype);
+Figure_S.prototype.constructor = Figure_S;
+
+
+function Figure_Z(posX, posY, color) {
+    Figure.apply(this, arguments);
+    this.slug[0].setPosX_Y(posX - 1, posY);
+    this.slug[1].setPosX_Y(posX, posY);
+    this.slug[2].setPosX_Y(posX, posY + 1);
+    this.slug[3].setPosX_Y(posX + 1, posY + 1);
+
+    this.map[0] = [{
+        posX: -1, posY: 0
+    },
+    { posX: 0, posY: -1 },
+    { posX: 1, posY: 0 },
+    { posX: 2, posY: -1 },
+    ];
+    this.map[1] = [{
+        posX: 1, posY: 0
+    },
+    { posX: 0, posY: 1 },
+    { posX: -1, posY: 0 },
+    { posX: -2, posY: 1 },
+    ];
+
+}
+
+Figure_Z.prototype = Object.create(Figure.prototype);
+Figure_Z.prototype.constructor = Figure_Z;
